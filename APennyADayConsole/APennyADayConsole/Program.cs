0bcf28a293f4068c965fd260e14cab92fff9d4c4
carl-halliburton <carl.j.halliburton@gmail.com>
2018-02-07 12:44:57 +1300

    added a sentinal to allow the app to run continuously

--- APennyADayConsole/APennyADayConsole/Program.cs
+++ APennyADayConsole/APennyADayConsole/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 
-namespace APennyADayConsole
+namespace APennyADayConsoleApp
 {
     class Program
     {
@@ -13,27 +13,35 @@ namespace APennyADayConsole
             long pennyTotal = 1;
             double dollarValue;
 
-            Console.WriteLine("Enter number of days: ");
+            //max 62
+            Console.WriteLine("Enter number of days up to 62 (x to exit): ");
             input = Console.ReadLine();
 
-            numDays = Convert.ToInt64(input);
-
-            while (count != numDays)
+            while (input != "x")
             {
-                pennyTotal = pennyTotal + pennyTotal;
-                Console.WriteLine("cur " + pennyTotal);
-                count++;
+                pennyTotal = 1;
+                count = 0;
+
+                numDays = Convert.ToUInt64(input);
+                Console.WriteLine("days " + numDays);
+                while (count != numDays)
+                {
+                    pennyTotal = pennyTotal + pennyTotal;
+                    Console.WriteLine("cur " + pennyTotal);
+                    count++;
+                }
+
+                dollarValue = pennyTotal / 100.0;
+
+                output = Math.Round(dollarValue, 2).ToString();
+
+                Console.WriteLine("Total in cents is: " + pennyTotal);
+                Console.WriteLine("Total in Dollars is: $" + output);
+
+                //max 62
+                Console.WriteLine("Enter number of days up to 62 (x to exit): ");
+                input = Console.ReadLine();
             }
-
-            dollarValue = pennyTotal / 100.0;
-
-            output = Math.Round(dollarValue, 2).ToString();
-
-            Console.WriteLine("Total in cents is: " + pennyTotal);
-            Console.WriteLine("Total in Dollars is: $" + output);
-
-            Console.WriteLine("Press Any Key to Continue...");
-            Console.ReadKey(true);
         }
     }
 }
