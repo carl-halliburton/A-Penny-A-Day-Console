10ddd102dcc0f8915e578ea6db1fb329e545761d
carl-halliburton <carl.j.halliburton@gmail.com>
2018-02-07 12:41:29 +1300

    app reads num days and calculates totla number of pennys or one cent, print the numbr of cents and dollars

--- APennyADayConsole/APennyADayConsole/Program.cs
+++ APennyADayConsole/APennyADayConsole/Program.cs
@@ -6,7 +6,34 @@ namespace APennyADayConsole
     {
         static void Main(string[] args)
         {
-            
+            string input;
+            string output;
+            long count = 0;
+            long numDays = 0;
+            long pennyTotal = 1;
+            double dollarValue;
+
+            Console.WriteLine("Enter number of days: ");
+            input = Console.ReadLine();
+
+            numDays = Convert.ToInt64(input);
+
+            while (count != numDays)
+            {
+                pennyTotal = pennyTotal + pennyTotal;
+                Console.WriteLine("cur " + pennyTotal);
+                count++;
+            }
+
+            dollarValue = pennyTotal / 100.0;
+
+            output = Math.Round(dollarValue, 2).ToString();
+
+            Console.WriteLine("Total in cents is: " + pennyTotal);
+            Console.WriteLine("Total in Dollars is: $" + output);
+
+            Console.WriteLine("Press Any Key to Continue...");
+            Console.ReadKey(true);
         }
     }
 }
